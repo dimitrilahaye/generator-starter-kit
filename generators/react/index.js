091ee1057157009a6e38d72647ca4941afbd3345
dimitrilahaye <contact@dimitrilahaye.net>
2020-10-24 18:18:37 +0200

    remove old react generators and create one react generator

--- generators/react/index.js
+++ generators/react/index.js
@@ -4,7 +4,11 @@ const chalk = require('chalk');
 module.exports = class extends BaseGenerator {
     constructor(args, opts) {
         super(args, opts);
-        this.root = 'react-vanilla';
+        // yo starter-kit:react --ts
+        this.option("ts");
+        // yo starter-kit:react --npm
+        this.option("npm");
+        this.root = 'react';
     }
 
     initializing() {
@@ -20,6 +24,20 @@ module.exports = class extends BaseGenerator {
                     name: 'applicationName',
                     required: true,
                     message: `What is the name of your ${this.root} application?`
+                },
+                {
+                    when: () => this.options.ts === undefined,
+                    type: 'confirm',
+                    name: 'typescript',
+                    required: true,
+                    message: 'Would you to use typescript?'
+                },
+                {
+                    when: () => this.options.npm === undefined,
+                    type: 'confirm',
+                    name: 'npm',
+                    required: true,
+                    message: 'Would you to install and build project?'
                 }
             ]);
         }
@@ -27,6 +45,12 @@ module.exports = class extends BaseGenerator {
 
     configuring() {
         this.info(`Configuring ${this.root}`);
+        if (this.options.ts) {
+            this.answers.typescript = true;
+        }
+        if (this.options.npm) {
+            this.answers.npm = true;
+        }
     }
 
     writing() {
@@ -44,14 +68,31 @@ module.exports = class extends BaseGenerator {
         // =======================================================
         // Doing templating stuff
         // =======================================================
-        const templates = ['src/App.js', 'package.json'];
-        templates.forEach((template) => this.writeFile(template));
+        const typescript = this.options.ts || this.answers.typescript;
+        this.fs.copyTpl(
+            this.destinationPath(applicationName, 'templates', typescript ? 'typescript' : 'javascript', 'src'),
+            this.destinationPath(applicationName, 'src', '..'),
+            this.answers
+        );
+        this.fs.copyTpl(
+            this.destinationPath(applicationName, 'templates', 'package.json'),
+            this.destinationPath(applicationName, 'package.json'),
+            this.answers
+        );
+        if (typescript) {
+            this.fs.copyTpl(
+                this.destinationPath(applicationName, 'templates', 'typescript', 'tsconfig.json'),
+                this.destinationPath(applicationName, 'tsconfig.json'),
+                this.answers
+            );
+        }
         this.fs.delete(this.destinationPath(applicationName, 'templates'));
     }
 
     async install() {
         const { rush } = this.getBaseConfiguration();
-        if (!rush) {
+        const npm = this.options.npm || this.answers.npm;
+        if (!rush && npm) {
             this.info(`install ${this.root}`);
             const { applicationName } = this.answers;
             await this.spawnCommandAsync('npm', ['install'], { cwd: this.destinationPath(applicationName) });
--- generators/react/templates/templates/typescript/src/App.tsx
+++ generators/react/templates/templates/typescript/src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          WELCOME IN <%= applicationName %>
+          Learn <%= applicationName %>
         </a>
       </header>
     </div>
